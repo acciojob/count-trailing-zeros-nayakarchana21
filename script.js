000a41cde23fdd015b941b901cc76e4cb0b232b7
Archana Nayak <archananayak212002@gmail.com>
2025-06-17 22:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
+  let count = 0;
+
+  while (n >= 5) {
+    n = Math.floor(n / 5);
+    count += n;
+  }
+
+  return count;
 }
 
-const input = prompt("Enter a number");
+const input = parseInt(prompt("Enter a number"), 10);
 alert(trailingZeros(input));
+
+
